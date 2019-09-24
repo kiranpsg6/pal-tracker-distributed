1845448d2680f2829d417eaaf0782355e2af5480
kiranpsg6 <kiran.psg6@gmail.com>
2019-09-24 12:25:58 +0530

    SSO and OAUTH changes

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -9,15 +9,8 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
 
-
 @Configuration
 public class RestConfig {
-
-    @Bean
-    @LoadBalanced
-    public RestOperations restOperations() {
-        return new RestTemplate();
-    }
 
     @Bean
     public ObjectMapper objectMapper() {
